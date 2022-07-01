9f837c8cf2fbbd857e8b98709408e481f1a20c0e
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-01 14:42:54 +0200

    Initial commit

---
+++ src/test/java/com/example/petClinicApp/PetClinicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petClinicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
