76a9e3e7e6e5929e5e0efd8f145656f8ee2f58b7
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-12 16:29:07 +0200

    Refactoring to multi-module project Closes #1

--- pet-clinic-web/src/main/java/com/example/petClinicApp/model/PetClinicAppApplication.java
+++ pet-clinic-web/src/main/java/com/example/petClinicApp/model/PetClinicAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.petClinicApp;
+package com.example.petClinicApp.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- pet-clinic-web/src/test/java/petClinicApp/PetClinicAppApplicationTests.java
+++ pet-clinic-web/src/test/java/petClinicApp/PetClinicAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.petClinicApp;
+package petClinicApp;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
