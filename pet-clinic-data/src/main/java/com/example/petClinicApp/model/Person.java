b3cf15619d8de62058c1be69adf01777d4815fd3
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-13 11:56:47 +0200

    Adding base entity. Closes  #10

--- pet-clinic-data/src/main/java/com/example/petClinicApp/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/petClinicApp/model/Person.java
@@ -1,6 +1,6 @@
 package com.example.petClinicApp.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
     private String firstName;
     private String lastName;
--- pet-clinic-data/src/main/java/com/example/petClinicApp/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/petClinicApp/model/PetType.java
@@ -1,6 +1,6 @@
 package com.example.petClinicApp.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String name;
 
