3ad368bd9fd20ba20a54c052d3f77df1f4e5902f
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-18 13:05:37 +0200

    Adding list of Owners. Closes #16

--- pet-clinic-web/src/main/java/com/example/petClinicApp/model/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petClinicApp/model/controllers/OwnerController.java
@@ -1,14 +1,23 @@
 package com.example.petClinicApp.model.controllers;
 
+import com.example.petClinicApp.model.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners","/owners/index","owners/index.html"})
-    public String listOfOwners()
+    public String listOfOwners(Model model)
     {
+        model.addAttribute("owners",ownerService.findALL());
         return "owners/index";
     }
 }
