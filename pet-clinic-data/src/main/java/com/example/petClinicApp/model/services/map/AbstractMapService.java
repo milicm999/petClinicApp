5434db7d5c7d878e57ba56f016b24b11c639828a
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-18 14:10:51 +0200

    Closes #23

--- pet-clinic-data/src/main/java/com/example/petClinicApp/model/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/example/petClinicApp/model/services/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package com.example.petClinicApp.model.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.example.petClinicApp.model.BaseEntity;
 
-public abstract class AbstractMapService<T,ID> {
+import java.util.*;
 
-    protected Map<ID,T> map=new HashMap<>();
+public abstract class AbstractMapService<T extends BaseEntity,ID extends Long> {
+
+    protected Map<Long,T> map=new HashMap<>();
 
     Set<T> findAll()
     {
@@ -17,9 +16,17 @@ public abstract class AbstractMapService<T,ID> {
     {
         return map.get(id);
     }
-    T save(ID id,T object)
+    T save(T object)
     {
-        map.put(id,object);
+        if(object!=null) {
+            if (object.getId() == null) {
+                object.setId(getNextId());
+            }
+            map.put(object.getId(),object);
+        }
+        else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
     void deleteById(ID id)
@@ -28,16 +35,19 @@ public abstract class AbstractMapService<T,ID> {
     }
     void deleteByObject(T object)
     {
-//        int id;
-//        for(int i=0;i<map.size();i++)
-//        {
-//            if(object==map.get(i)) {
-//                id = i;
-//                map.remove(id);
-//            }
-//        }
-
         map.entrySet().removeIf(entry -> entry.getValue().equals(object));
+    }
+    private Long getNextId()
+    {
+        Long nextId=null;
+        try {
+            nextId=Collections.max(map.keySet()) +1;
+        }
+        catch (NoSuchElementException e){
+            nextId=1L;
+        }
+
+        return nextId;
     }
 
 }
--- pet-clinic-web/src/main/java/com/example/petClinicApp/model/bootstrap/LoadData.java
+++ pet-clinic-web/src/main/java/com/example/petClinicApp/model/bootstrap/LoadData.java
@@ -23,14 +23,12 @@ public class LoadData implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1=new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Maja");
         owner1.setLastName("Milic");
 
         ownerService.save(owner1);
 
         Owner owner2=new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Nemanja");
         owner2.setLastName("Despotovic");
 
@@ -39,14 +37,12 @@ public class LoadData implements CommandLineRunner {
         System.out.println("Owners are loaded...");
 
         Vet vet1= new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Goran");
         vet1.setLastName("Vasiljevic");
 
         vetService.save(vet1);
 
         Vet vet2= new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Dragan");
         vet2.setLastName("Zivanovic");
 
