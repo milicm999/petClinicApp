729763048737035411ba623689e15f18aec62a99
Marija999 <105052720+milicm999@users.noreply.github.com>
2022-07-18 13:34:40 +0200

    Added list for Vets. Closes #15

--- pet-clinic-web/src/main/java/com/example/petClinicApp/model/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/petClinicApp/model/controllers/VetController.java
@@ -1,14 +1,24 @@
 package com.example.petClinicApp.model.controllers;
 
+import com.example.petClinicApp.model.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
+
     @RequestMapping({"/vets","vets/index","vets","/vets/index.html"})
-    public String listVets()
+    public String listVets(Model model)
     {
+        model.addAttribute("vets",vetService.findALL());
         return "vets/index";
     }
 }
